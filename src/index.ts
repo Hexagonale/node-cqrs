1aefd33c4de9c0d90056b0d5e0b5a1f481223fec
Przemysław Kędzierski <przemekkt@gmail.com>
2024-09-29 22:57:18 +0200

    Separate logger out

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,13 @@ import 'source-map-support/register';
 import cors from 'cors';
 import express from 'express';
 import { configFactory } from './config.factory';
-import pino from 'pino';
 import { MongoClient } from 'mongodb';
+import { logger } from './logger';
 
 const main = async () => {
-	const logger = pino(
-		pino.transport({
-			level: 'debug',
-			target: 'pino-pretty',
-			options: { colorize: true },
-		})
-	);
-
 	const config = configFactory();
 
-	const mongo = await MongoClient.connect(config.mongoUrl);
-
+	const mongoClient = await MongoClient.connect(config.mongoUrl);
 	logger.info(`Connected to MongoDB on "${config.mongoUrl}"`);
 
 	const app = express();
